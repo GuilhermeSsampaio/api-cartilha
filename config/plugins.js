c6aeddeb65b140f454e7fad786317ee213ac761c
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-09-26 10:46:36 -0400

    email funcionando

--- config/plugins.js
+++ config/plugins.js
@@ -18,8 +18,8 @@ module.exports = ({env}) => ({
       config: {
         provider: 'nodemailer',
         providerOptions: {
-          host: env('SMTP_HOST', 'smtp.example.com'),
-          port: env('SMTP_PORT', 587),
+          host: env('SMTP_HOST'),
+          port: env('SMTP_PORT'),
           auth: {
             user: env('SMTP_USERNAME'),
             pass: env('SMTP_PASSWORD'),
@@ -27,8 +27,8 @@ module.exports = ({env}) => ({
           // ... any custom nodemailer options
         },
         settings: {
-          defaultFrom: 'hello@example.com',
-          defaultReplyTo: 'hello@example.com',
+          defaultFrom: 'no-reply@strapi.io',
+          defaultReplyTo: 'no-reply@strapi.io',
         },
       },
     },
