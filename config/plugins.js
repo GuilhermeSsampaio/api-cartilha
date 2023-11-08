cebb0a24c2e2ca00f5ed9fb494eaa52201f59a3e
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-11-08 10:44:30 -0300

    imagens indo pro minio

--- config/plugins.js
+++ config/plugins.js
@@ -1,37 +1,20 @@
 module.exports = ({env}) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: 'strapi-provider-upload-minio-ce',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        show:{},
-        delete: {},
+        accessKey: env('MINIO_ACCESS_KEY', 'Q3AM3UQ867SPQQA43P2F'),
+        secretKey: env('MINIO_SECRET_KEY', 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'),
+        bucket: env('MINIO_BUCKET', 'test-2021-09-22'),
+        endPoint: env('MINIO_ENDPOINT', 'play.min.io'),
+        port: env('MINIO_PORT', 9000),
+        useSSL: env('MINIO_USE_SSL', true),
+        folder: env('MINIO_FOLDER', 'cms'),
+        private: env('MINIO_PRIVATE', false), // private bucket
+        expiry: env('MINIO_EXPIRY', 7 * 24 * 60 * 60), // default 7 days, unit: seconds, only work for private bucket
       },
     },
   },
-  // email: {
-  //   config: {
-  //     provider: 'nodemailer',
-  //     providerOptions: {
-  //       host: env('SMTP_HOST'),
-  //       port: env('SMTP_PORT'),
-  //       auth: {
-  //         user: env('SMTP_USERNAME'),
-  //         pass: env('SMTP_PASSWORD'),
-  //       },
-  //       // ... any custom nodemailer options
-  //     },
-  //     settings: {
-  //       defaultFrom: 'no-reply@strapi.io',
-  //       defaultReplyTo: 'no-reply@strapi.io',
-  //     },
-  //   },
-  // },
   email: {
     config: {
       provider: 'sendgrid',
