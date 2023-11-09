83b5ee2a6fe292cc35d3679e243c108398713309
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-11-09 10:54:01 -0400

    correção para o minio

--- config/plugins.js
+++ config/plugins.js
@@ -3,15 +3,15 @@ module.exports = ({env}) => ({
     config: {
       provider: 'strapi-provider-upload-minio-ce',
       providerOptions: {
-        accessKey: env('MINIO_ACCESS_KEY', 'Q3AM3UQ867SPQQA43P2F'),
-        secretKey: env('MINIO_SECRET_KEY', 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG'),
-        bucket: env('MINIO_BUCKET', 'test-2021-09-22'),
-        endPoint: env('MINIO_ENDPOINT', 'play.min.io'),
-        port: env('MINIO_PORT', 9000),
-        useSSL: env('MINIO_USE_SSL', true),
-        folder: env('MINIO_FOLDER', 'cms'),
-        private: env('MINIO_PRIVATE', false), // private bucket
-        expiry: env('MINIO_EXPIRY', 7 * 24 * 60 * 60), // default 7 days, unit: seconds, only work for private bucket
+        accessKey: env('MINIO_ACCESS_KEY'),
+        secretKey: env('MINIO_SECRET_KEY'),
+        bucket: env('MINIO_BUCKET'),
+        endPoint: env('MINIO_ENDPOINT'),
+        port: env('MINIO_PORT'),
+        useSSL: env('MINIO_USE_SSL'),
+        folder: env('MINIO_FOLDER'),
+        private: env('MINIO_PRIVATE'), // private bucket
+        expiry: env('MINIO_EXPIRY'), // default 7 days, unit: seconds, only work for private bucket
       },
     },
   },
