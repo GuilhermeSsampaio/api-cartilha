6e7f2d72a4a4b2df744b7ff246f59918030a8147
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2024-02-06 15:12:48 -0400

    subindo pro railway

--- config/database.js
+++ config/database.js
@@ -1,15 +1,31 @@
-module.exports = ({env}) => ({
-  connection:{
+// module.exports = ({env}) => ({
+//   connection:{
+//     client: 'postgres',
+//     connection:{
+//         host: env('DATABASE_HOST'),
+//         port: env('DATABASE_PORT'),
+//         database: env('DATABASE_NAME'),
+//         user: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//         ssl: env.bool('DATABASE_SSL')
+//     }
+
+//   }
+
+// })
+
+
+module.exports = ({ env }) => ({
+  connection: {
     client: 'postgres',
-    connection:{
-        host: env('DATABASE_HOST'),
-        port: env('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL')
-    }
-
-  }
-
-})
+    connection: {
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'strapi'),
+      user: env('PGUSER', 'strapi'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
+    },
+    pool: { min: 0 }
+  },
+});
